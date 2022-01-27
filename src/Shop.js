edfe41f018d6b7ad9b39b6032bb737563054184c
SuraZz007 <surajdev9886@gmail.com>
2022-01-27 17:19:12 +0530

    templates made for actions and reducers

--- src/Shop.js
+++ src/Shop.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Shop = () => {
   return( 
   <div className='my-2'>
-      <h2>Buy adidas shoes for Rs.50</h2>
+      <h2> Deposit/Withdraw money</h2>
     <button className="btn btn-primary mx-2 my-2">-</button>
-        Add to cart.
+       Update Balance
     <button className="btn btn-primary mx-2">+</button>
 
   </div>
